7a5fd880926cea118480103489d795d44b82296a
Laura <lauravelazquez.lnv@gmail.com>
2018-10-20 16:32:28 -0300

    lichenReport

---
+++ lichenapp/src/main/java/org/cloudhack/lichenapp/LichenReport.java
@@ -0,0 +1,31 @@
+package org.cloudhack.lichenapp;
+
+import lombok.RequiredArgsConstructor;
+
+/**
+ *
+ * @author Larita
+ */
+@AllArgscConstructor
+public class LichenReport {
+      public String picture;
+      public int linchen;
+      public int porcent;
+      public int location;
+     
+      public String getPicture(){
+          return picture;
+      }
+      public int getLinchen(){
+          return picture;
+      }
+       public int getPorcent(){
+          return picture;
+      }
+        public int getLocation(){
+          return picture;
+      }
+
+}
+
+
