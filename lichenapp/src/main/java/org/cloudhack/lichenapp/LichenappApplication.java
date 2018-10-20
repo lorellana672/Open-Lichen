df69c754df30a5c7a15594cb52141dd38268227e
unknown <lmorellana672@gmail.com>
2018-10-20 14:39:28 -0300

    code structure

---
+++ lichenapp/src/main/java/org/cloudhack/lichenapp/LichenappApplication.java
@@ -0,0 +1,12 @@
+package org.cloudhack.lichenapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LichenappApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LichenappApplication.class, args);
+	}
+}
